09e4e8c94c3420f3f0d0494120d03c03ad4739db
Jean-Loup <jean-loup.caillet@u-space.fr>
2023-10-13 11:32:00 +0200

    Adding documentation

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Git-Toolkit
+# GitLab Cloner
+
+GitLab Cloner is a simple GUI application that facilitates the process of cloning multiple repositories from a specific GitLab group. Simply provide your personal access token, specify the group name, and select the projects you wish to clone.
+
+## Features
+
+- **Token and Group Input**: Simply enter your GitLab personal access token and the name of the group containing the repositories you want to clone.
+- **Project Selection**: Once the group's projects are listed, you can select multiple projects to clone.
+- **Select All Button**: With just one click, select all the projects listed.
+- **Clone Progress**: A progress bar displays the cloning progress, updating after each repository is cloned.
+
+## Requirements
+
+- Python
+- GTK+ 3
+- Requests library
+
+## Installation
+
+1. Ensure you have Python and GTK+ 3 installed on your system.
+2. Install the required Python libraries:
+   ```bash
+   pip install requests PyGObject
+
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/AoNovae/Git-Toolkit.git
+2. Navigate to the repository
+    ```bash
+    cd Git-Toolkit
+
+## Usage
+
+1. Run the script:
+    ```bash
+    python clone_all.py
+2. Enter your GitLab personal access token in the "Enter Token" field.
+3. Enter the desired group name in the "Enter Group Name" field.
+4. Click "Check" to fetch and display the projects in that group.
+5. Select the projects you wish to clone (or use the "Select All" button to select all projects).
+6. Choose the directory where the projects should be cloned.
+7. Click "Clone" to start the cloning process. The progress bar will update as each repository is cloned.
+
+
+## Licence 
+
+This project is open source. Feel free to use, modify, and distribute as you see fit.
+
--- clone_all.py
+++ clone_all.py
@@ -18,6 +18,16 @@ The class uses the `requests` library to interact with the GitLab API and the `s
 
 class GitLabCloner(Gtk.Window):
     def __init__(self):
+        """
+        Initializes the GitLabCloner GUI window and sets up the user interface elements.
+
+        Inputs:
+        - None
+
+        Outputs:
+        - UI window with GitLabCloner elements
+        """
+
         Gtk.Window.__init__(self, title="GitLab Cloner")
         self.set_border_width(10)
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
@@ -31,7 +41,7 @@ class GitLabCloner(Gtk.Window):
         self.group_entry.set_placeholder_text("Enter Group Name")
         vbox.pack_start(self.group_entry, False, False, 0)
 
-        self.check_button = Gtk.Button(label="Vérifier")
+        self.check_button = Gtk.Button(label="Check")
         self.check_button.connect("clicked", self.on_check_clicked)
         vbox.pack_start(self.check_button, False, False, 0)
 
@@ -68,9 +78,23 @@ class GitLabCloner(Gtk.Window):
         vbox.pack_start(self.progress_bar, False, False, 0)
 
     def on_cell_toggled(self, widget, path):
+        """
+        Toggles the value of the first column in the selected row of the TreeView's model.
+
+        :param widget: The widget that triggered the signal (toggle button).
+        :param path: The path of the selected row in the TreeView.
+        :return: None
+        """
         self.projects_store[path][0] = not self.projects_store[path][0]
 
     def on_check_clicked(self, widget):
+        """
+        Retrieves the GitLab access token and group name entered by the user, calls the `get_projects` method to fetch the projects from the GitLab API, and updates the TreeView with the retrieved projects.
+
+        :param widget: The widget that triggered the signal (the "Check" button).
+        :type widget: Gtk.Button
+        :return: None
+        """
         token = self.token_entry.get_text()
         group_name = self.group_entry.get_text()
         projects = self.get_projects(token, group_name)
@@ -81,12 +105,32 @@ class GitLabCloner(Gtk.Window):
             self.projects_store.append([False, project[0], project[1]])
 
     def get_projects(self, token, group_name):
+        """
+        Retrieves the projects from a GitLab group.
+
+        Args:
+            token (str): The GitLab access token.
+            group_name (str): The name of the GitLab group.
+
+        Returns:
+            list: A list of tuples containing the project name and SSH URL to the repository for each project in the GitLab group.
+        """
         group_id = self.get_group_id(token, group_name)
         if group_id:
             return self.get_projects_from_group(token, group_id)
         return []
 
     def get_group_id(self, token, group_name):
+        """
+        Retrieves the ID of a GitLab group based on the provided access token and group name.
+
+        Args:
+            token (str): The GitLab access token.
+            group_name (str): The name of the GitLab group.
+
+        Returns:
+            int or None: The ID of the GitLab group, or None if the group was not found.
+        """
         headers = {"PRIVATE-TOKEN": token}
         response = requests.get(
             f"{GITLAB_URL}/api/v4/groups?search={group_name}", headers=headers
@@ -96,6 +140,16 @@ class GitLabCloner(Gtk.Window):
         return None
 
     def get_projects_from_group(self, token, group_id):
+        """
+        Retrieves the projects from a GitLab group based on the provided access token and group ID.
+
+        Args:
+            token (str): The GitLab access token.
+            group_id (int): The ID of the GitLab group.
+
+        Returns:
+            list: A list of tuples containing the project name and SSH URL for each project in the GitLab group.
+        """
         headers = {"PRIVATE-TOKEN": token}
         response = requests.get(
             f"{GITLAB_URL}/api/v4/groups/{group_id}/projects?per_page=100",
@@ -109,6 +163,14 @@ class GitLabCloner(Gtk.Window):
         return []
 
     def on_clone_clicked(self, widget):
+        """
+        Clone selected projects from GitLab to a specified directory and update the progress bar.
+
+        :param widget: The widget that triggered the signal (the "Clone" button).
+        :type widget: Gtk.Widget
+        :return: None
+        """
+
         clone_dir = self.dir_chooser.get_filename()
 
         total_projects = sum(1 for row in self.projects_store if row[0])
@@ -125,10 +187,37 @@ class GitLabCloner(Gtk.Window):
                     Gtk.main_iteration_do(False)
 
     def on_select_all_clicked(self, widget):
+        """
+        Selects all the projects in the TreeView by setting the value of the first column in each row of the model to True.
+
+        :param self: The instance of the GitLabCloner class.
+        :param widget: The widget that triggered the signal (the "Select All" button).
+        :return: None
+        """
         for row in self.projects_store:
             row[0] = True
 
     def clone_project(self, project_url, clone_dir):
+        """
+        Clones a GitLab project by executing the `git clone` command.
+
+        Args:
+            project_url (str): The URL of the GitLab project to clone.
+            clone_dir (str): The directory where the project will be cloned.
+
+        Returns:
+            None
+
+        Raises:
+            subprocess.CalledProcessError: If the `git clone` command fails.
+
+        Example:
+            # Initialize the GitLabCloner class object
+            win = GitLabCloner()
+
+            # Call the clone_project method with the project URL and clone directory
+            win.clone_project("https://gitlab.com/example/project.git", "/path/to/clone/directory")
+        """
         project_name = project_url.split("/")[-1].replace(".git", "")
         full_clone_path = f"{clone_dir}/{project_name}"
         try:
@@ -137,7 +226,14 @@ class GitLabCloner(Gtk.Window):
             pass  # Handle the error as appropriate for your application
 
 
-win = GitLabCloner()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+if __name__ == "__main__":
+    """
+    Entry point of the program.
+
+    Creates an instance of the `GitLabCloner` class, connects the "destroy" signal to the `Gtk.main_quit` function,
+    shows the GUI window, and starts the GTK main loop.
+    """
+    win = GitLabCloner()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
